c8534b68884d4c5c474ceb6334f0a4f7dc8d27d1
Unknown <lpc477@gmail.com>
2019-02-09 12:06:24 +0000

    Got Homepage running
    Username:admin
    Password:adminadmin
    Added blog app and art app and home.html
    Set up media directory

--- ArtGallery/ArtGallery/urls.py
+++ ArtGallery/ArtGallery/urls.py
@@ -1,4 +1,4 @@
-"""gallery URL Configuration
+"""ArtGallery URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf import settings
 
-import gallery.views
+import art.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',gallery.views.home,name = 'home'),
+    path('',art.views.home, name='home'),
 ]
