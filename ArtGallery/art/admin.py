d93236d26142fd2df9058944e69d36d23782e504
Unknown <lpc477@gmail.com>
2019-02-19 14:37:50 +0000

    Models added to admin page

--- ArtGallery/art/admin.py
+++ ArtGallery/art/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-#from .models import Art #makes job model visible to admin page
+from .models import Picture #makes job model visible to admin page
 
-#admin.site.register(Art)
+admin.site.register(Picture)
 
 #register not working right now, look at the media setup
