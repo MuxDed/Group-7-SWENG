e22fc29a5010e8b9140d5158e98a330ce373524b
Unknown <lpc477@gmail.com>
2019-03-05 13:20:30 +0000

    Implemented PostgreSQL Server
    Still needs to be tested, but assumes it will work. Server username and password are the same

--- ArtGallery/ArtGallery/settings.py
+++ ArtGallery/ArtGallery/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ArtGallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sweng',
+        'USER': 'postgres',
+        'PASSWORD' : 'lpc477',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
