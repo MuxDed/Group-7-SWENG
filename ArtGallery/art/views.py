1381b07fffb713b3e5efba9bb54497baa893e8ac
Unknown <lpc477@gmail.com>
2019-03-08 15:59:40 +0000

    Testing the post
    I'm having trouble getting the home.html to retrieve the associated image, may be that not commiting the photo is the issue, will submit a test image now to see...

--- ArtGallery/art/views.py
+++ ArtGallery/art/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Picture
 
 def home(request):
-    #arts = Art.objects
-    return render(request, 'art/home.html')
+    arts = Picture.objects
+    return render(request, 'art/home.html', {'arts':arts})
 
 def portfolio(request):
     return render(request, 'art/portfolio.html')
